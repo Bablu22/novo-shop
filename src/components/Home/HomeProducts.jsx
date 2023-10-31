c8098603df614d7c5137984c61d7c02fe9db6306
Bablu Mia <72483554+Bablu22@users.noreply.github.com>
2023-10-31 19:21:23 +0600

    Update HomeProducts.jsx

--- src/components/Home/HomeProducts.jsx
+++ src/components/Home/HomeProducts.jsx
@@ -41,7 +41,7 @@ const HomeProducts = () => {
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
             {productsByCategory[categoryName]
-              
+              .slice(0, productsToShow)
               .map((product) => (
                 <ProductCard
                   key={product._id}
